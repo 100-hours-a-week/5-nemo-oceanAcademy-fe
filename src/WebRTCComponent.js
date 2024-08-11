acfdf42e37cfd0489b3c91544e354ac41c5f903d
sodaakim <sohuikim2020@naver.com>
2024-08-12 00:47:09 +0900

    feat cicd

--- src/WebRTCComponent.js
+++ src/WebRTCComponent.js
@@ -32,6 +32,7 @@ const WebRTCComponent = () => {
     <div>
       <h1>WebRTC 테스트</h1>
       <h5>안녕하세요 저희는 니모예요 저희의 바다서원을 많이 사랑해주세요</h5>
+      <h5>주디의 CICD 완성! 이제 HTTPS 해볼게용</h5>
       <video ref={videoRef} autoPlay playsInline />
     </div>
   );
