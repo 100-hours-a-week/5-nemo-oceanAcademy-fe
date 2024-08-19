a8b3cf8d0e02194e315961dbb29d7243613f9efe
mia (Yuna) <yuna00708@gmail.com>
2024-08-19 21:52:30 +0900

    :sparkles: feat: Signup Info Page Layout

--- src/pages/user/mypage/MyPage.tsx
+++ src/pages/user/mypage/MyPage.tsx
@@ -1,3 +1,5 @@
+// 사용자 페이지 
+
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import LectureCard from '../../../components/lecture-card/LectureCard';
--- src/pages/user/sign-info/SignInfo.tsx
+++ src/pages/user/sign-info/SignInfo.tsx
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './SignInfo.module.css';
 
 const SignInfo: React.FC = () => {
+  const [preview, setPreview] = useState<string | null>(null);
+  const [nickname, setNickname] = useState<string>('');
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file && (file.type === 'image/jpeg' || file.type === 'image/png') && file.size <= 5 * 1024 * 1024) {
+      const reader = new FileReader();
+      reader.onloadend = () => setPreview(reader.result as string);
+      reader.readAsDataURL(file);
+    } else {
+      alert('Invalid file. Please select a .jpg or .png file under 5MB.');
+    }
+  };
+
   return (
-    <div>
-      <h1>레이아웃 채워질 예정</h1>
+    <div className={styles.container}>
+      <h2 className={styles.title}>추가 정보 입력</h2>
+
+      <div className={styles.imageContainer}>
+        <label className={styles.label}>프로필 사진(선택)</label>
+        <input
+          type="file"
+          id="fileInput"
+          style={{ display: 'none' }}
+          onChange={handleFileChange}
+        />
+        <button
+          className={styles.uploadButton}
+          onClick={() => document.getElementById('fileInput')?.click()}
+        >
+          {preview ? (
+            <img src={preview} alt="Preview" className={styles.previewImage} />
+          ) : (
+            '+'
+          )}
+        </button>
+        <ul className={styles.bulletList}>
+          <li className={styles.listItem}>사진은 1개만 업로드할 수 있습니다.</li>
+          <li className={styles.listItem}>파일 사이즈는 100*100을 권장합니다.</li>
+          <li className={styles.listItem}>파일 크기는 5MB 이하만 가능합니다.</li>
+          <li className={styles.listItem}>파일은 .jpg, .png만 업로드할 수 있습니다.</li>
+          <li className={styles.listItem}>사진을 업로드하지 않을 경우 기본 프로필로 설정됩니다.</li>
+        </ul>
+      </div>
+
+      <div className={styles.nicknameContainer}>
+        <label className={styles.label}>
+            닉네임<span className={styles.requiredMark}>*</span>
+          </label>
+          <p className={styles.helperText}>필수 입력 사항입니다.</p>
+          <input
+            type="text"
+            className={styles.inputField}
+            placeholder="닉네임을 설정해주세요."
+            value={nickname}
+            onChange={(e) => setNickname(e.target.value)}
+          />
+      </div>
     </div>
   );
 };
