08586f159c52d9f3f78676b7de20a15743a4daef
mia (Yuna) <yuna00708@gmail.com>
2024-08-24 19:23:27 +0900

    :sparkles: feat: Live Card Component and Live Lecture List Page api connection and design

--- src/components/lecture-card/LiveCard.tsx
+++ src/components/lecture-card/LiveCard.tsx
@@ -3,16 +3,24 @@ import { useNavigate } from 'react-router-dom';
 import styles from './LiveCard.module.css';
 
 interface LiveCardProps {
-  title: string;
+  classId: number;
+  name: string;
+  bannerImage: string;
   instructor: string;
   category: string;
 }
 
-const LiveCard: React.FC<LiveCardProps> = ({ title, instructor, category }) => {
+const LiveCard: React.FC<LiveCardProps> = ({ classId, bannerImage, name, instructor, category }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/lecture/info?id=${classId}`);
+  };
+
   return (
-    <div className={styles.card}>
-      <div className={styles.liveBox}>live 강의 화면</div>
-      <h2 className={styles.title}>{title}</h2>
+    <div className={styles.container} onClick={handleClick}>
+      <div className={styles.banner} style={{ backgroundImage: `url(${bannerImage})` }}></div>
+      <h2 className={styles.title}>{name}</h2>
       <p className={styles.details}>
         {instructor} | {category}
       </p>
