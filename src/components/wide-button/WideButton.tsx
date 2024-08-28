727461a2126c282c90a043f2ddab779eff36ce69
mia (Yuna) <yuna00708@gmail.com>
2024-08-28 16:12:58 +0900

    :sparkles: feat: api 경로 수정

--- src/components/wide-button/WideButton.tsx
+++ src/components/wide-button/WideButton.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import styles from './WideButton.module.css';
 
 interface WideButtonProps {
-    text: string;
-    onClick?: () => void;
-    disabled?: boolean;
+  text: string;
+  onClick?: () => void;
+  disabled?: boolean;
+  fixed?: boolean;
 }
 
-const WideButton: React.FC<WideButtonProps> = ({ text, onClick, disabled }) => {
+const WideButton: React.FC<WideButtonProps> = ({ text, onClick, disabled, fixed }) => {
   return (
     <button 
-        className={styles.wideButton} 
+        className={`${styles.wideButton} ${fixed ? styles.fixed : ''}`} // fixed 속성에 따라 클래스 추가
         onClick={onClick}
         disabled={disabled}
     >
--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,13 +1,13 @@
 // #A-1: Main (/) - 메인 화면/랜딩페이지 
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import styles from './Main.module.css';
 import Advertisement from '../../components/advertisement/Advertisement';
 import LectureCard from '../../components/lecture-card/LectureCard';
 import Navigation from '../../components/navigation/Navigation';
 import axios from 'axios';
 import endpoints from '../../api/endpoints';
-import bn from '../../assets/images/ad_big0.png';
+import styles from './Main.module.css';
+import { Empty } from '../../styles/GlobalStyles';
 
 interface Lecture {
   classId: number;
@@ -23,33 +23,11 @@ const Main: React.FC = () => {
   const [topTenClasses, setTopTenClasses] = useState<Lecture[]>([]); 
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
-
-    // TODO: 더미 데이터 삭제하고 
-    const dummyyClasses = [
-      { classId: 11, name: '헤이즐리의 아트 클래스', bannerImage: bn, instructor: '헤이즐리', category: '아트' },
-      { classId: 12, name: '토니의 작명소', bannerImage: bn, instructor: '토니', category: '작문' },
-      { classId: 13, name: '주디 너무 귀여워요', bannerImage: bn, instructor: '주디', category: '프로그래밍' },
-      { classId: 14, name: '스티븐은 지렁이', bannerImage: bn, instructor: '스티븐', category: '운동' },
-    ]
-
-    const dummyClasses = [
-      { classId: 1, name: '모바일 앱 개발 실습', bannerImage: bn, instructor: '첸', category: '개발' },
-      { classId: 2, name: '기타 코드 마스터 클래스', bannerImage: bn, instructor: '실비아', category: '음악' },
-      { classId: 3, name: '초상화 그리기 워크숍', bannerImage: bn, instructor: '테오', category: '미술' },
-      { classId: 4, name: '프랑스 요리의 비밀', bannerImage: bn, instructor: '앤디', category: '요리' },
-      { classId: 5, name: '요가로 시작하는 하루', bannerImage: bn, instructor: '엘', category: '운동' },
-      { classId: 6, name: '초보자를 위한 DSLR 강의', bannerImage: bn, instructor: '미아', category: '사진' },
-      { classId: 7, name: '프랑스어 발음 마스터', bannerImage: bn, instructor: '준', category: '외국어' },
-      { classId: 8, name: 'UX/UI 디자인의 기본', bannerImage: bn, instructor: '에리카', category: '디자인' },
-      { classId: 9, name: '천문학 입문', bannerImage: bn, instructor: '제이미', category: '과학' },
-      { classId: 10, name: '리더십 기초 트레이닝', bannerImage: bn, instructor: '홍', category: '비즈니스 스킬' },
-      { classId: 11, name: '미모 켠왕', bannerImage: bn, instructor: '릴리', category: '과학' }
-    ]
   
   // 데이터를 가져오는 공통 함수
   const fetchLectures = async (target: string, setState: React.Dispatch<React.SetStateAction<Lecture[]>>) => {
     try {
-      const response = await axios.get(`${endpoints.getLectures}?target=${target}`);
+      const response = await axios.get(`${endpoints.classes}?target=${target}`);
       const classes = response.data.classes.map((item: any) => ({
         classId: item.class_id,
         name: item.name,
@@ -83,14 +61,17 @@ const Main: React.FC = () => {
         </section>
 
         <section className={styles.liveSection}>
-          <h1 className={styles.sectionTitle}>
-            인기를 끌고 있는 라이브 강의!<br/> 지금 바로 수강 레쯔ㅋ고
-          </h1>
-          <span className={styles.link} onClick={() => navigate('/live-list')}>
-            라이브 중인 강의 보러 가기 &gt;
-          </span>
+          <Empty height="10px" />
+          <div className={styles.titleSection}>
+            <h1 className={styles.sectionTitle}>
+              인기를 끌고 있는 라이브 강의!<br/> 지금 바로 수강 레쯔ㅋ고
+            </h1>
+            <span className={styles.link} onClick={() => navigate('/live-list')}>
+              라이브 중인 강의 보러 가기 &gt;
+            </span>
+          </div>
           <div className={styles.lectureGrid}>
-            {dummyyClasses.slice(0, 4).map((lecture) => (
+            {liveClasses.slice(0, 4).map((lecture) => (
               <LectureCard 
                 key={lecture.classId}
                 classId={lecture.classId}
@@ -104,12 +85,14 @@ const Main: React.FC = () => {
         </section>
 
         <section className={styles.toptenSection}>
-          <h1 className={styles.sectionTitle}>수강생이 많은 강의 Top10</h1>
-          <span className={styles.link} onClick={() => navigate('/list')}>
-            전체 강의 보러 가기 &gt;
-          </span>
+          <div className={styles.titleSection}>
+            <h1 className={styles.sectionTitle}>수강생이 많은 강의 Top10</h1>
+            <span className={styles.link} onClick={() => navigate('/list')}>
+              전체 강의 보러 가기 &gt;
+            </span>
+          </div>
           <div className={styles.lectureGrid}>
-            {dummyClasses.map((lecture) => (
+            {topTenClasses.map((lecture) => (
               <LectureCard 
                 key={lecture.classId} 
                 classId={lecture.classId}
