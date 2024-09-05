e6e2d17b1ea817c5ad719fa0d31af3bfc1456a05
mia (Yuna) <yuna00708@gmail.com>
2024-09-05 17:33:36 +0900

    :truck: refactor: 배포용 uri

--- src/pages/user/login/Login.tsx
+++ src/pages/user/login/Login.tsx
@@ -49,7 +49,7 @@ const Login: React.FC = () => {
         }
 
         window.Kakao.Auth.authorize({
-            redirectUri: 'http://www.nemooceanacademy.com/oauth/kakao/callback',
+            redirectUri: 'https://www.nemooceanacademy.com/oauth/kakao/callback',
         });
     };
 
