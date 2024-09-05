d8133e79c68bd6ff2d22b319d44230ac6f2023a3
mia (Yuna) <yuna00708@gmail.com>
2024-09-05 17:24:43 +0900

    :bug: fix: redirect Uri edited

--- src/pages/user/login/Login.tsx
+++ src/pages/user/login/Login.tsx
@@ -49,7 +49,7 @@ const Login: React.FC = () => {
         }
 
         window.Kakao.Auth.authorize({
-            redirectUri: 'https://www.nemooceanacademy.com/oauth/kakao/callback',
+            redirectUri: 'http://www.nemooceanacademy.com/oauth/kakao/callback',
         });
     };
 
