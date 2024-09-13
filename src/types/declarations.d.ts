f39275225d1563dd0e711d4bb3acf682c16c0180
mia (Yuna) <yuna00708@gmail.com>
2024-09-13 09:31:31 +0900

    :recycle: chore: type 정리 합침

--- src/types/declarations.d.ts
+++ src/types/declarations.d.ts
@@ -1,6 +1,26 @@
 // src/types/declarations.d.ts
 
 declare module '*.module.css' {
-    const classes: { [key: string]: string };
-    export default classes;
-  }
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.gif' {
+  const value: string;
+  export default value;
+}
