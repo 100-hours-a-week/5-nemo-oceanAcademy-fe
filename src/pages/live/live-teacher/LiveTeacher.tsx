0a5cd4f46cb8ad433ed1e78e4148dca33bfeb259
mia (Yuna) <yuna00708@gmail.com>
2024-09-16 18:32:04 +0900

    :sparkles: feat: sendMessages 변경

--- src/pages/live/live-teacher/LiveTeacher.tsx
+++ src/pages/live/live-teacher/LiveTeacher.tsx
@@ -171,9 +171,10 @@ const LiveTeacher: React.FC = () => {
   const sendMessage = () => {
     if (stompClient && stompClient.connected) {
       const chatMessage = {
-        roomId: currentRoom, 
+        roomId: currentRoom,
         content: content,
         writer: userInfo ? userInfo.nickname : '누구세요',
+        profile_image_path: userInfo?.profileImage,
         createdDate: new Date().toISOString()
       };
 
